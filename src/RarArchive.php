55c75aa3037aad716b08e3dc5fc13d5890f2eaf3
Vadim (-FeLiX-) Seluyanov <felicast@mail.ru>
2014-09-26 21:23:05 +0400

    annotations

--- src/RarArchive.php
+++ src/RarArchive.php
@@ -1,16 +1,24 @@
 <?php
 namespace 
 {
-	class RarArchive
-	{
-		public function open($filename, $password = null, $volume_callback = null) {}
-		public function getEntries() {}
-		public function getEntry($filename) {}
-		public function isSolid() {}
-		public function getComment() {}
-		public function isBroken() {}
-		public function setAllowBroken($allow_broken) {}
-		public function close() {}
-		public function __toString() {}
-	}
+    class RarArchive
+    {
+        public function open($filename, $password = null, $volume_callback = null) {}
+
+        /**
+         * @return \RarEntry[]
+         */
+        public function getEntries() {}
+
+        /**
+         * @return \RarEntry
+         */
+        public function getEntry($filename) {}
+        public function isSolid() {}
+        public function getComment() {}
+        public function isBroken() {}
+        public function setAllowBroken($allow_broken) {}
+        public function close() {}
+        public function __toString() {}
+    }
 }
